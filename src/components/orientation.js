0c705c940a1219140059fcc2f00b8fbede22172a
Wynn Digital <wynndigital@gmail.com>
2019-06-30 19:59:20 -0400

    complete orientation section

--- src/components/orientation.js
+++ src/components/orientation.js
@@ -3,9 +3,11 @@ import { Container } from 'reactstrap';
 
 import styles from '../styles/orientation.module.css';
 
+import finePrintImage from '../images/admissions-fine-print.png';
+
 const Orientation = () => {
     return (
-        <section>
+        <section className={styles.orientationSection}>
             <Container>
                 <h2>New Class/Orientation</h2>
                 <hr />
@@ -43,6 +45,27 @@ const Orientation = () => {
                     </div>
                 </div>
                 <p>* Class Orientation is mandatory for all new students on or before signing the enrollment agreement. Please bring the follwing: Driver's License, Social Security Card, High School Diploma/GED</p>
+                <p>KCK Beauty and Barber offer the following affordable programs of Study:</p>
+                <ul>
+                    <li>Barber: 1200 Hours; Program Cost: $12,875.00</li>
+                    <li>Cosmetology: 1200 Hours; Program Cost: $12,875.00</li>
+                    <li>Full Specialist: 600 Hours; Program Cost: $6,980.00</li>
+                    <li>Instructor: 600 Hours; Program Cost: $4,750.00</li>
+                    <li>Manicure: 240 Hours; Program Cost: $3,175.00</li>
+                    <li>Skin Care Specialist: 260 Hours; Program Cost: $3,530.00</li>
+                </ul>
+                <p className={styles.removeMargin}>Financial Aid is available for Cosmetology Students who qualify.</p>
+                <p className={styles.removeMargin}>Federal School Code: 042768</p>
+                <p className={styles.removeMargin}>Day and Evening Hours available to accommodate Full and Part time Schedules</p>
+                <p>Admission office is open M-F 8:30-4:30. Please call to schedule a tour.</p>
+
+                <h5><u>Additional Disclosures</u></h5>
+                <p className={styles.removeMargin}>Reporting Period 2018</p>
+                <p className={styles.removeMargin}>Graduation Rate - 80.00%</p>
+                <p className={styles.removeMargin}>Placement Rate - 87.50%</p>
+                <p className={styles.removeMargin}>Licensure Rate - 80.00%</p>
+
+                <img className={styles.finePrint} src={finePrintImage} alt=""/>
             </Container>
         </section>
     )
