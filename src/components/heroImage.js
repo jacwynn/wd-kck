c5c5c9ce149669e6548346ab87a10986fdd1f802
Wynn Digital <wynndigital@gmail.com>
2019-06-29 21:44:06 -0400

    update markup on heroImage component

--- src/components/heroImage.js
+++ src/components/heroImage.js
@@ -7,14 +7,16 @@ import { Container, Row, Col, Button } from 'reactstrap';
 const HeroImage = () => {
     return (
         <section className={styles.heroImage}>
-            <div className={styles.wrap}>
-                <div className={styles.imageContainer}>
-                    <img src={image} />
+            <Container>
+                <div className={styles.wrap}>
+                    <div className={styles.imageContainer}>
+                        <img src={image} />
+                    </div>
+                    <div className={styles.textContainer}>
+                        <h4>If you are looking to unleash your artistic and creative side while taking charge of your professional career in the Beauty and Barber industry. Look no further.</h4>
+                    </div>
                 </div>
-                <div className={styles.textContainer}>
-                    <h4>If you are looking to unleash your artistic and creative side while taking charge of your professional career in the Beauty and Barber industry. Look no further.</h4>
-                </div>
-            </div>
+            </Container>
         </section>
     )
 }
