024facb76a1b98c35cdc92b04cbdb141f89cc2aa
Wynn Digital <wynndigital@gmail.com>
2019-06-29 22:40:15 -0400

    make content block component reusable with props

--- src/components/contentBlock.js
+++ src/components/contentBlock.js
@@ -3,14 +3,15 @@ import { Container, Row, Col, Button } from 'reactstrap';
 
 import styles from '../styles/contentBlock.module.css';
 
-const ContentBlock = () => {
+const ContentBlock = (props) => {
     return (
         <section className={styles.contentBlockSection}>
             <Container>
-                <h2 className={styles.headline}>Why KCK?</h2>
+                <h2 className={styles.headline}>{props.title}</h2>
                 <hr />
-                <p>At KCK Beauty & Barber Academy Inc., we recognize that the demand for creative and well trained professionals in the beauty and barber industry is growing daily. We offer dynamic programs in a modern facility to successfully train and prepare you for a limitless career. Our students not only learn technical skills needed in their chosen field of study but interpersonal, professional and business fundamentals as well. Our Instructors are well versed in the use of innovative equipment and effective techniques to present and enhance the school’s state approved curriculum.</p>
-                <p>KCK Beauty & Barber Academy Inc. is the place where students are able to pursue their passion, unleash their skills, creativity and be equipped with the tools and knowledge required to succeed.</p>
+                {props.paragraphs.map((paragraph, index) => {
+                    return <p key={index}>{paragraph}</p>
+                })}
             </Container>
         </section>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -8,12 +7,20 @@ import HeroImage from '../components/heroImage';
 import Programs from '../components/programs';
 import ContentBlock from '../components/contentBlock';
 
+const content = {
+  title: "Why KCK?",
+  paragraphs: [
+      "At KCK Beauty & Barber Academy Inc., we recognize that the demand for creative and well trained professionals in the beauty and barber industry is growing daily. We offer dynamic programs in a modern facility to successfully train and prepare you for a limitless career. Our students not only learn technical skills needed in their chosen field of study but interpersonal, professional and business fundamentals as well. Our Instructors are well versed in the use of innovative equipment and effective techniques to present and enhance the school’s state approved curriculum.",
+      "KCK Beauty & Barber Academy Inc. is the place where students are able to pursue their passion, unleash their skills, creativity and be equipped with the tools and knowledge required to succeed."
+  ]
+}
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HeroImage />
     <Programs />
-    <ContentBlock />
+    <ContentBlock title={content.title} paragraphs={content.paragraphs} />
   </Layout>
 )
 
