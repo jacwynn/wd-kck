4e6adc958064afe24332b5f733043a19b8b72b0c
Wynn Digital <wynndigital@gmail.com>
2019-06-29 22:13:34 -0400

    move bootstrap file to layout.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,19 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 import HeroImage from '../components/heroImage';
 import Programs from '../components/programs';
+import ContentBlock from '../components/contentBlock';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HeroImage />
     <Programs />
-        
-
+    <ContentBlock />
   </Layout>
 )
 
