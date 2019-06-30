e3a8f57b6175ddf0014b4662b63c06b3c6ad3041
Wynn Digital <wynndigital@gmail.com>
2019-06-29 21:46:24 -0400

    create programs component

---
+++ src/components/programs.js
@@ -0,0 +1,81 @@
+import React from "react"
+import { Card, Container, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+import styles from '../styles/programs.module.css';
+
+// Images
+import barberIMG from '../images/hp-barber.jpeg'
+import cosmetologyIMG from '../images/hp-cosmetology.jpg';
+import skinCareIMG from '../images/hp-skin_care.jpg';
+import manicureIMG from '../images/hp-manicure.jpg';
+import specialistIMG from '../images/hp-specialist.jpg';
+import instructorIMG from '../images/hp-instructor.jpg';
+
+const Programs = () => {
+    return (
+        <section className={styles.programSection}>
+            <Container>
+            <h2 className={styles.headline}>Welcome to KCK Beauty & Barber Academy Inc.</h2>
+
+                <div className={styles.programs}>
+                    <a href="#">
+                        <Card className={styles.card}>
+                            <CardImg top width="100%" src={barberIMG} alt="KCK Barber Program" />
+                            <CardBody>
+                                <CardTitle className={styles.cardTitle}>Barber Program</CardTitle>
+                            </CardBody>
+                        </Card>
+                    </a>
+
+                    <a href="#">
+                        <Card className={styles.card}>
+                            <CardImg top width="100%" src={cosmetologyIMG} alt="KCK Cosmetology Program" />
+                            <CardBody>
+                                <CardTitle className={styles.cardTitle}>Cosmetology Program</CardTitle>
+                            </CardBody>
+                        </Card>
+                    </a>
+
+                    <a href="#">
+                        <Card className={styles.card}>
+                            <CardImg top width="100%" src={skinCareIMG} alt="KCK Skin Care Program" />
+                            <CardBody>
+                                <CardTitle className={styles.cardTitle}>Skin Care Program</CardTitle>
+                            </CardBody>
+                        </Card>
+                    </a>
+
+                    <a href="#">
+                        <Card className={styles.card}>
+                            <CardImg top width="100%" src={manicureIMG} alt="KCK Manicure Program" />
+                            <CardBody>
+                                <CardTitle className={styles.cardTitle}>Manicure Program</CardTitle>
+                            </CardBody>
+                        </Card>
+                    </a>
+
+                    <a href="#">
+                        <Card className={styles.card}>
+                            <CardImg top width="100%" src={specialistIMG} alt="KCK Full Specialist Program" />
+                            <CardBody>
+                                <CardTitle className={styles.cardTitle}>Full Specialist Program</CardTitle>
+                            </CardBody>
+                        </Card>
+                    </a>
+
+                    <a href="#">
+                        <Card className={styles.card}>
+                            <CardImg top width="100%" src={instructorIMG} alt="KCK Instructor Program" />
+                            <CardBody>
+                                <CardTitle className={styles.cardTitle}>Instructor Program</CardTitle>
+                            </CardBody>
+                        </Card>
+                    </a>
+                </div>
+            </Container>
+        </section>
+    )
+}
+
+export default Programs
