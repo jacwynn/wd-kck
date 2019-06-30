461f6c7455c615efc52524fc29e5cad3989890b2
Wynn Digital <wynndigital@gmail.com>
2019-06-29 21:45:45 -0400

    add programs component to index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,13 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 import HeroImage from '../components/heroImage';
+import Programs from '../components/programs';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HeroImage />
-    
+    <Programs />
         
 
   </Layout>
