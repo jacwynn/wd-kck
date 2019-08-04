09c4865a6d941a3c01a21581cdc0017e8d88486b
Wynn Digital <wynndigital@gmail.com>
2019-08-04 16:27:43 -0400

    add hyperlinks to content

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,6 +8,9 @@ import Staff from '../components/staff';
 
 import vaIMG from '../images/vabenefits-stamp.png';
 
+const hyperlinks = {
+    naccas: "<a href='http://www.naccas.org' target='_blank'>www.naccas.org</a>"
+}
 
 const contentAbout = {
     title: "About KCK Beauty & Barber Academy",
@@ -21,15 +24,14 @@ const contentApproval = {
     title: "Approvals",
     paragraphs: [
         "KCK Beauty and Barber Academy is approved by the State of Florida as a training Institution to qualify graduates for State Licensing as Barbers, Cosmetologist, Manicurist or Skin Care Specialist. We are licensed by the Commission for Independent Education and Florida Department of Education.",
-        "KCK Beauty & Barber Academy is accredited by the National Accrediting Commission of Career Arts and Sciences (NACCAS), 3015 Colvin Street, Alexandria, VA 22314 -www.naccas.org"
+        <div dangerouslySetInnerHTML={{__html: 'KCK Beauty & Barber Academy is accredited by the National Accrediting Commission of Career Arts and Sciences (NACCAS), 3015 Colvin Street, Alexandria, VA 22314 <a href="http://www.naccas.org" target="_blank">-www.naccas.org</a>'}} />
     ]
 }
 
 const contentVABenefits = {
     title: "VA Benefits",
     paragraphs: [
-        "KCK Beauty and Barber Academy is approved by the State of Florida as a training Institution to qualify graduates for State Licensing as Barbers, Cosmetologist, Manicurist or Skin Care Specialist. We are licensed by the Commission for Independent Education and Florida Department of Education.",
-        "KCK Beauty & Barber Academy is accredited by the National Accrediting Commission of Career Arts and Sciences (NACCAS), 3015 Colvin Street, Alexandria, VA 22314 -www.naccas.org"
+        <div dangerouslySetInnerHTML={{__html: 'KCK Beauty and Barber Academy, Inc is approved by US Department of Veteran Affairs for Veterans Training for Education assistance of Veterans, dependents of Veterans and Survivors of Veterans. Call today to speak to our financial director for more information regarding Veteran Benefits. Helpful link to refer to <a href="https://www.va.gov/" target="_blank">www.va.gov</a>'}} />
     ]
 }
 
