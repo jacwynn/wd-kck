adde4f0e1c0cbc637c98849550fdbc5ac8eb19a5
Wynn Digital <wynndigital@gmail.com>
2019-08-30 23:13:07 -0400

    adjust header container

--- src/components/header.js
+++ src/components/header.js
@@ -32,70 +32,72 @@ export default class Header extends React.Component {
   render() {
     return (
       <div className={styles.header}>
-        <Navbar dark expand="md">
-          <NavbarToggler onClick={this.toggle} />
-          <p className={styles.logoText}>KCK Beauty and Barber Academy</p>
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav navbar className="ml-0 w-100 justify-content-between">
-              <NavItem className="mb-0">
-                <NavLink href="/" className={styles.headerNavLink}>Home</NavLink>
-              </NavItem>
-              <NavItem className="mb-0">
-                <NavLink href="/about" className={styles.headerNavLink}>About Us</NavLink>
-              </NavItem>
-              <UncontrolledDropdown className="mb-0" nav inNavbar>
-                <DropdownToggle nav caret className={styles.headerNavLink}>
-                  Students
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem className={styles.dropdownLinksBtn}>
-                    <Link to="/admissions" className={styles.dropdownLinks}>Admission</Link>
-                  </DropdownItem>
-                  <DropdownItem className={styles.dropdownLinksBtn}>
-                    <Link to="/catalog" className={styles.dropdownLinks}>School Catalog</Link>
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              <NavItem className="mb-0">
-                <NavLink href="/financial-aid" className={styles.headerNavLink}>Financial Aid</NavLink>
-              </NavItem>
-              <UncontrolledDropdown className="mb-0" nav inNavbar>
-                <DropdownToggle nav caret className={styles.headerNavLink}>
-                  Programs
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem className={styles.dropdownLinksBtn}>
-                    <Link to="/barber-program" className={styles.dropdownLinks}>Barber Program</Link>
-                  </DropdownItem>
-                  <DropdownItem className={styles.dropdownLinksBtn}>
-                    <Link to="/cosmetology-program" className={styles.dropdownLinks}>Cosmetology Program</Link>
-                  </DropdownItem>
-                  <DropdownItem className={styles.dropdownLinksBtn}>
-                    <Link to="/manicure-program" className={styles.dropdownLinks}>Manicure Program</Link>
-                  </DropdownItem>
-                  <DropdownItem className={styles.dropdownLinksBtn}>
-                    <Link to="/skin-care-program" className={styles.dropdownLinks}>Skin Care Specialist Program</Link>
-                  </DropdownItem>
-                  <DropdownItem className={styles.dropdownLinksBtn}>
-                    <Link to="/full-specialist-program" className={styles.dropdownLinks}>Full Specialist Program</Link>
-                  </DropdownItem>
-                  <DropdownItem className={styles.dropdownLinksBtn}>
-                    <Link to="/instructor-program" className={styles.dropdownLinks}>Instructor Program</Link>
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              <NavItem className="mb-0">
-                <NavLink href="/services" className={styles.headerNavLink}>Services</NavLink>
-              </NavItem>
-              <NavItem className="mb-0">
-                <NavLink href="/va-benefits" className={styles.headerNavLink}>VA Benefits</NavLink>
-              </NavItem>
-              <NavItem className="mb-0">
-                <NavLink href="/contact" className={styles.headerNavLink}>Contact Us</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
+        <div class="container" className={styles.headerContainer}>
+          <Navbar dark expand="md">
+            <NavbarToggler onClick={this.toggle} />
+            <p className={styles.logoText}>KCK Beauty and Barber Academy</p>
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav navbar className="ml-0 w-100 justify-content-between">
+                <NavItem className="mb-0">
+                  <NavLink href="/" className={styles.headerNavLink}>Home</NavLink>
+                </NavItem>
+                <NavItem className="mb-0">
+                  <NavLink href="/about" className={styles.headerNavLink}>About Us</NavLink>
+                </NavItem>
+                <UncontrolledDropdown className="mb-0" nav inNavbar>
+                  <DropdownToggle nav caret className={styles.headerNavLink}>
+                    Students
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem className={styles.dropdownLinksBtn}>
+                      <Link to="/admissions" className={styles.dropdownLinks}>Admission</Link>
+                    </DropdownItem>
+                    <DropdownItem className={styles.dropdownLinksBtn}>
+                      <Link to="/catalog" className={styles.dropdownLinks}>School Catalog</Link>
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+                <NavItem className="mb-0">
+                  <NavLink href="/financial-aid" className={styles.headerNavLink}>Financial Aid</NavLink>
+                </NavItem>
+                <UncontrolledDropdown className="mb-0" nav inNavbar>
+                  <DropdownToggle nav caret className={styles.headerNavLink}>
+                    Programs
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem className={styles.dropdownLinksBtn}>
+                      <Link to="/barber-program" className={styles.dropdownLinks}>Barber Program</Link>
+                    </DropdownItem>
+                    <DropdownItem className={styles.dropdownLinksBtn}>
+                      <Link to="/cosmetology-program" className={styles.dropdownLinks}>Cosmetology Program</Link>
+                    </DropdownItem>
+                    <DropdownItem className={styles.dropdownLinksBtn}>
+                      <Link to="/manicure-program" className={styles.dropdownLinks}>Manicure Program</Link>
+                    </DropdownItem>
+                    <DropdownItem className={styles.dropdownLinksBtn}>
+                      <Link to="/skin-care-program" className={styles.dropdownLinks}>Skin Care Specialist Program</Link>
+                    </DropdownItem>
+                    <DropdownItem className={styles.dropdownLinksBtn}>
+                      <Link to="/full-specialist-program" className={styles.dropdownLinks}>Full Specialist Program</Link>
+                    </DropdownItem>
+                    <DropdownItem className={styles.dropdownLinksBtn}>
+                      <Link to="/instructor-program" className={styles.dropdownLinks}>Instructor Program</Link>
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+                <NavItem className="mb-0">
+                  <NavLink href="/services" className={styles.headerNavLink}>Services</NavLink>
+                </NavItem>
+                <NavItem className="mb-0">
+                  <NavLink href="/va-benefits" className={styles.headerNavLink}>VA Benefits</NavLink>
+                </NavItem>
+                <NavItem className="mb-0">
+                  <NavLink href="/contact" className={styles.headerNavLink}>Contact Us</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
       </div>
     );
   }
