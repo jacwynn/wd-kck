e89c583cb83d06d0be50d8cd7079e3afb29094f3
Wynn Digital <wynndigital@gmail.com>
2019-08-31 20:37:09 -0400

    update contact form

--- src/components/footer.js
+++ src/components/footer.js
@@ -32,54 +32,45 @@ export default class Footer extends React.Component {
                 <Modal isOpen={this.state.modal} toggle={this.toggle}>
                     <ModalHeader toggle={this.toggle}>Request Information Form</ModalHeader>
                     <ModalBody>
-                        <Form name="SignUpForm" method="POST" data-netlify="true">
+                        <form name="SignUpForm" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
                             <input type="hidden" name="bot-field" />
-                            <input type="hidden" name="form-name" value="SignUpForm" />
-                            <FormGroup>
-                                <Label for="exampleEmail">First Name</Label>
-                                <Input type="text" name="firstName" id="exampleEmail" />
-                            </FormGroup> 
-
-                            <FormGroup>
-                                <Label for="exampleEmail">Last Name</Label>
-                                <Input type="text" name="lastName" id="exampleEmail" />
-                            </FormGroup> 
-
-                            <FormGroup>
-                                <Label for="exampleEmail">Email</Label>
-                                <Input type="email" name="email" id="exampleEmail" />
-                            </FormGroup> 
-
-                            <FormGroup>
-                                <Label for="exampleEmail">Phone Number</Label>
-                                <Input type="tel" name="phoneNumber" id="exampleEmail" />
-                            </FormGroup> 
-
-                            <FormGroup>
-                                <Label for="exampleEmail">Address</Label>
-                                <Input type="text" name="address" id="exampleEmail" />
-                            </FormGroup> 
-
-                            <FormGroup>
-                                <Label for="exampleEmail">State</Label>
-                                <Input type="text" name="state" id="exampleEmail" />
-                            </FormGroup> 
-
-                            <FormGroup>
-                                <Label for="exampleEmail">City</Label>
-                                <Input type="text" name="city" id="exampleEmail" />
-                            </FormGroup> 
-
-                            <FormGroup>
-                                <Label for="exampleEmail">Zip Code</Label>
-                                <Input type="text" name="zipCode" id="exampleEmail" />
-                            </FormGroup> 
-
-                            <FormGroup>
-                                <Label for="exampleEmail">Questions or Comments</Label>
-                                <Input type="textarea" name="questions" id="exampleEmail" />
-                            </FormGroup> 
-                        </Form>                       
+                            <div class="form-group">
+                                <label>First Name</label>
+                                <input type="text" class="form-control" name="firstName"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Last Name</label>
+                                <input type="text" class="form-control" name="lastName"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Email</label>
+                                <input type="email" class="form-control" name="email"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Phone Number</label>
+                                <input type="tel" class="form-control" name="phoneNumber"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Address</label>
+                                <input type="tel" class="form-control" name="address"/>
+                            </div>
+                            <div class="form-group">
+                                <label>State</label>
+                                <input type="tel" class="form-control" name="state"/>
+                            </div>
+                            <div class="form-group">
+                                <label>City</label>
+                                <input type="tel" class="form-control" name="city"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Zip Code</label>
+                                <input type="text" class="form-control" name="zipCode"/>
+                            </div>
+                            <div class="form-group">
+                                <label>Questions/Comments?</label>
+                                <textarea type="type" class="form-control" name="comments"/>
+                            </div>
+                        </form>
                     </ModalBody>
                     <ModalFooter>
                         <Button type="submit" color="danger" onClick={this.toggle}>Get Information</Button>{' '}
