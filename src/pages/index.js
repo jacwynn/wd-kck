3c1ab81cda234e134575db9f9a2f29405da58e75
Wynn Digital <wynndigital@gmail.com>
2019-08-31 20:59:53 -0400

    update contact form

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ const IndexPage = () => (
     <HeroImage />
     <Programs />
     <ContentBlock title={content.title} paragraphs={content.paragraphs} />
-    {/* <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+    <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
               <input type="hidden" name="bot-field" />
               <div className="field half first">
                   <label htmlFor="name">Name</label>
@@ -39,52 +39,9 @@ const IndexPage = () => (
                   <li><input type="submit" value="Send Message" className="special" /></li>
                   <li><input type="reset" value="Clear" /></li>
               </ul>
-          </form> */}
+          </form>
 
-<form name="SignUpForm" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-                            <input type="hidden" name="bot-field" />
-                            <div class="form-group">
-                                <label>First Name</label>
-                                <input type="text" class="form-control" name="firstName"/>
-                            </div>
-                            <div class="form-group">
-                                <label>Last Name</label>
-                                <input type="text" class="form-control" name="lastName"/>
-                            </div>
-                            <div class="form-group">
-                                <label>Email</label>
-                                <input type="email" class="form-control" name="email"/>
-                            </div>
-                            <div class="form-group">
-                                <label>Phone Number</label>
-                                <input type="text" class="form-control" name="phoneNumber"/>
-                            </div>
-                            <div class="form-group">
-                                <label>Address</label>
-                                <input type="text" class="form-control" name="address"/>
-                            </div>
-                            <div class="form-group">
-                                <label>State</label>
-                                <input type="text" class="form-control" name="state"/>
-                            </div>
-                            <div class="form-group">
-                                <label>City</label>
-                                <input type="text" class="form-control" name="city"/>
-                            </div>
-                            <div class="form-group">
-                                <label>Zip Code</label>
-                                <input type="text" class="form-control" name="zipCode"/>
-                            </div>
-                            <div class="form-group">
-                                <label>Questions/Comments?</label>
-                                <textarea type="type" class="form-control" name="comments"/>
-                            </div>
-                            <input type="submit" value="Send Message" className="special" />
-                            {/* <ModalFooter>
-                                <Button type="submit" color="danger" onClick={this.toggle}>Get Information</Button>{' '}
-                                <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-                            </ModalFooter> */}
-                        </form>
+
   </Layout>
 )
 
